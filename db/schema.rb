5d68e3a0a4a84c29ceeca572f2e015f3d218e7a0
Brad Dunlap <braddunlapdpt@gmail.com>
2023-02-01 16:56:07 -0500

    created Items database

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_01_205622) do
+ActiveRecord::Schema.define(version: 2023_02_01_215351) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.float "price"
+    t.boolean "featured"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "restaurant_id"
+    t.index ["restaurant_id"], name: "index_items_on_restaurant_id"
+  end
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -23,4 +33,5 @@ ActiveRecord::Schema.define(version: 2023_02_01_205622) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "items", "restaurants"
 end
