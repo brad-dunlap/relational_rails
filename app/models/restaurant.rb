fdc82070169a01b958aa6d9e47c471a7cc6355ef
Brad Dunlap <braddunlapdpt@gmail.com>
2023-02-06 15:37:57 -0500

    testing: Wrote model tests that were previously missing

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,7 @@
 class Restaurant < ApplicationRecord
 	has_many :items
+
+	def item_count
+		self.items.count
+	end
 end
--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -2,4 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Restaurant do
 	it {should have_many :items}
+
+	it 'displays the number of menu items associated with the restaurant' do
+		restaurant = Restaurant.create!(name: "Brads", num_of_employees: 2, alcohol_served: true)
+		item1 = restaurant.items.create(name: "Pierogies", price: 6.99, featured: true)
+		item2 = restaurant.items.create(name: "Pizza", price: 6.99, featured: true)
+		expect(restaurant.item_count).to eq(2)
+	end
 end
