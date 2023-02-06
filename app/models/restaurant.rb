33944c61224142d96d7d5580d77e4460f0e24990
Brad Dunlap <braddunlapdpt@gmail.com>
2023-02-06 16:41:32 -0500

    feat: moved order by to restaurants model and tested appropriately

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,10 @@
 class Restaurant < ApplicationRecord
 	has_many :items
 
+	scope :by_recently_created, -> { order(created_at: :desc) }
+
 	def item_count
 		self.items.count
 	end
+
 end
--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -3,10 +3,21 @@ require 'rails_helper'
 RSpec.describe Restaurant do
 	it {should have_many :items}
 
+	it 'displays restaurants in descending order by created_by' do
+		restaurant1 = Restaurant.create!(name: "Brads", num_of_employees: 2, alcohol_served: true)
+		restaurant2 = Restaurant.create!(name: "Dans", num_of_employees: 2, alcohol_served: true)
+		
+		restaurants = Restaurant.by_recently_created
+
+		expect(restaurants.first).to eq(restaurant2)
+    expect(restaurants.second).to eq(restaurant1)
+	end
+
 	it 'displays the number of menu items associated with the restaurant' do
 		restaurant = Restaurant.create!(name: "Brads", num_of_employees: 2, alcohol_served: true)
 		item1 = restaurant.items.create(name: "Pierogies", price: 6.99, featured: true)
 		item2 = restaurant.items.create(name: "Pizza", price: 6.99, featured: true)
+		
 		expect(restaurant.item_count).to eq(2)
 	end
 end
