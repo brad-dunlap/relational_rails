73c203c15a3c5e649bf4384ef756a572185c55d9
Brad Dunlap <braddunlapdpt@gmail.com>
2023-02-06 20:04:59 -0500

    refactor: cleanup of routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,23 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
 	get '/', to: 'welcome#index'
+	
 	get '/restaurants', to: 'restaurants#index'
 	post '/restaurants', to: 'restaurants#create'
 	get '/restaurants/new', to: 'restaurants#new'
+
 	get '/restaurants/:id', to: 'restaurants#show'
 	patch '/restaurants/:id', to: 'restaurants#update'
 	get '/restaurants/:id/edit', to: 'restaurants#edit'
 	get '/restaurants/:id/items', to: 'restaurant_items#index'
 	get '/restaurants/:id/items/new', to: 'restaurant_items#new'
 	post '/restaurants/:id/items', to: 'restaurant_items#create'
+	delete '/restaurants/:id', to: 'restaurants#destroy'
+
 	get '/items', to: 'items#index'
 	get '/items/:id', to: 'items#show'
 	get '/items/:id/edit', to: 'items#edit'
 	patch '/items/:id', to: 'items#update'
-	delete '/restaurants/:id', to: 'restaurants#destroy'
 	delete '/items/:id', to: 'items#destroy'
 
 end
